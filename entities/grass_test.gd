cf638eff373855b4b97224a28400ac665b65b521
F1dg3t <57208077+F1dg3tXD@users.noreply.github.com>
2026-08-21 12:36:51 +0600

    Add grass test sprites

---
+++ entities/grass_test.gd
@@ -0,0 +1,33 @@
+extends Node3D
+
+@onready var area_3d: Area3D = $Area3D
+@onready var animated_sprite_3d: AnimatedSprite3D = $AnimatedSprite3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animated_sprite_3d.play("wave_idle")
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if not (
+		body.is_in_group("player")
+		or body.is_in_group("friend")
+		or body.is_in_group("enemy")
+	):
+		return
+	animated_sprite_3d.play("squish")
+	await animated_sprite_3d.animation_finished
+	animated_sprite_3d.frame = animated_sprite_3d.sprite_frames.get_frame_count("squish") - 1
+	animated_sprite_3d.pause()
+
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if not (
+		body.is_in_group("player")
+		or body.is_in_group("friend")
+		or body.is_in_group("enemy")
+	):
+		return
+	animated_sprite_3d.play("spring")
+	await animated_sprite_3d.animation_finished
+	animated_sprite_3d.play("wave_idle")
